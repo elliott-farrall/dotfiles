3506a036a14b0e8ba180e21a8e91a5b385faac98
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-05 12:08:18 +0100

    refactor: Refactor file paths to use `config.xdg` variables
    - Updated `default.nix` file to use `config.xdg.configHome` instead of `config.xdg.stateHome` for the `dotDir` path
    - Modified `history.path` in `default.nix` to utilize `config.xdg.stateHome`

--- homes/x86_64-linux/elliott@elliotts-laptop/config/shell/zsh/default.nix
+++ homes/x86_64-linux/elliott@elliotts-laptop/config/shell/zsh/default.nix
@@ -6,8 +6,8 @@
 {
   programs.zsh = {
     enable = true;
-    dotDir = "${config.xdg.configHome}/zsh";
-    history.path = "${lib.strings.removePrefix config.home.homeDirectory config.xdg.stateHome}/zsh/history";
+    dotDir = "${lib.strings.removePrefix config.home.homeDirectory config.xdg.configHome}/zsh";
+    history.path = "${config.xdg.stateHome}/zsh/history";
     completionInit = ''
       [ -d "$XDG_CACHE_HOME"/zsh ] || mkdir -p "$XDG_CACHE_HOME"/zsh
       zstyle ':completion:*' cache-path "$XDG_CACHE_HOME"/zsh/zcompcache
