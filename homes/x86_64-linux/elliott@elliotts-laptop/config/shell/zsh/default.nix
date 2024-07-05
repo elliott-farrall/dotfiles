650a7612fe823fca9c5470e7356ef8146ec92b62
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-05 12:05:54 +0100

    chore: Update `history.path` in default.nix file
    - Update `history.path` in `shell/zsh/default.nix` to remove home directory prefix

--- homes/x86_64-linux/elliott@elliotts-laptop/config/shell/zsh/default.nix
+++ homes/x86_64-linux/elliott@elliotts-laptop/config/shell/zsh/default.nix
@@ -1,4 +1,5 @@
 { config
+, lib
 , ...
 }:
 
@@ -6,7 +7,7 @@
   programs.zsh = {
     enable = true;
     dotDir = "${config.xdg.configHome}/zsh";
-    history.path = "${config.xdg.stateHome}/zsh/history";
+    history.path = "${lib.strings.removePrefix config.home.homeDirectory config.xdg.stateHome}/zsh/history";
     completionInit = ''
       [ -d "$XDG_CACHE_HOME"/zsh ] || mkdir -p "$XDG_CACHE_HOME"/zsh
       zstyle ':completion:*' cache-path "$XDG_CACHE_HOME"/zsh/zcompcache
