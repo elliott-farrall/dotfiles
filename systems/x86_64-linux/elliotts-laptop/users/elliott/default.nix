74c67b9025f4f2c9495f791e253446fe39c118c7
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-09 21:06:16 +0100

    chore: Update user and group settings in default.nix
    - Add group "fuse" to the `extraGroups` of user "elliott" in default.nix

--- systems/x86_64-linux/elliotts-laptop/users/elliott/default.nix
+++ systems/x86_64-linux/elliotts-laptop/users/elliott/default.nix
@@ -6,6 +6,6 @@
     isNormalUser = true;
     uid = 1000;
     hashedPassword = "$6$oGzE.WsebpKGq6EL$H8jCPwUWgtG/YIsNBl1DTYuHBTUFiGWsXwQhUvHUuIT0DzL77esFfuH1oT6LdCNvCn.H2IpoKE3uP0h0.BIE01";
-    extraGroups = [ "networkmanager" "wheel" "docker" "openrazer" "adbusers" ];
+    extraGroups = [ "networkmanager" "wheel" "docker" "fuse" "openrazer" "adbusers" ];
   };
 }
