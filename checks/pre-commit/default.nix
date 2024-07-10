d09df9bd040ddafb07aba65f71b595d6fac605fb
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-10 15:04:29 +0100

    Improve pre-commit hooks and shell configuration inheritance
    - Enable various pre-commit hooks for code quality and spell checking
    - Add configuration for the `no-commit-to-branch` hook
    - Update `inherit` statement in `hyprwm/kitty` module
    - Modify `config.shell.zsh` module to inherit `enable` property

--- checks/pre-commit/default.nix
+++ checks/pre-commit/default.nix
@@ -1,5 +1,4 @@
 { lib
-, pkgs
 , ...
 }:
 
@@ -8,11 +7,27 @@ lib.pre-commit-hooks.x86_64-linux.run {
 
   hooks = {
     gptcommit.enable = true;
+    forbid-new-submodules.enable = true;
+    no-commit-to-branch.settings.branch = [ "main" ];
 
+    check-added-large-files.enable = true;
+    check-executables-have-shebangs.enable = true;
+    check-shebang-scripts-are-executable.enable = true;
+    detect-private-keys.enable = true;
     editorconfig-checker.enable = true;
+    end-of-file-fixer.enable = true;
+    trim-trailing-whitespace.enable = true;
+
+    hunspell.enable = true;
+    typos.enable = true;
 
     nil.enable = true;
     nixpkgs-fmt.enable = true;
     deadnix.enable = false;
+    statix.enable = false;
+
+    beautysh.enable = true;
+    shellcheck.enable = true;
+    shfmt.enable = true;
   };
 }
--- modules/nixos/shell/zsh/default.nix
+++ modules/nixos/shell/zsh/default.nix
@@ -6,7 +6,7 @@
 
 let
   cfg = config.shell.zsh;
-  enable = cfg.enable;
+  inherit (cfg) enable;
 in
 {
   options = {
