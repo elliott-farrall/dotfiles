0e6a48ca176a5ec84bb14c665668b21435e23295
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-10 17:06:44 +0100

    chore: Refactor boot configuration to improve system startup.
    - Update kernelParams in default.nix configuration file

--- systems/x86_64-linux/elliotts-laptop/config/boot/default.nix
+++ systems/x86_64-linux/elliotts-laptop/config/boot/default.nix
@@ -10,15 +10,17 @@
     plymouth.enable = true;
 
     kernelParams = [
-      "quiet"
-      "splash"
-      "bgrt_disable"
       "boot.shell_on_fail"
       "i915.fastboot=1"
+
+      "splash"
+      "bgrt_disable"
+
+      "quiet"
       "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
+      "udev.log_level=3"
       "udev.log_priority=3"
+      "systemd.show_status=false"
     ];
 
     loader = {
