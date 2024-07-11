5054130ff3d1a94634cecc0dcb45fefbb15ece32
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-11 17:38:49 +0100

    Update VSCode to use `code-insiders` and modify configurations
    - Change `vscode` package to `pkgs.vscode-insiders` in `default.nix`
    - Remove `nix-update` input and add `code-insiders` input in `flake.nix`
    - Update dependencies in `flake.lock`
    - Remove `nix-update` package from `packages` list in `default.nix`
    - Add new file `overlays/vscode-insiders/default.nix`
    - Modify `vscode` attribute and add `commandLineArgs` and `postInstall` properties in `overlays/vscode-insiders/default.nix`
    - Create symbolic link from `$out/bin/code-insiders` to `$out/bin/code` in `overlays/vscode-insiders/default.nix`
    - Modify `wrapProgram` command to include `--unset NIXOS_OZONE_WL` in `overlays/vscode-insiders/default.nix`

--- shells/default/default.nix
+++ shells/default/default.nix
@@ -10,14 +10,12 @@ mkShell {
   inherit (inputs.self.checks.x86_64-linux.pre-commit) shellHook;
   buildInputs = inputs.self.checks.x86_64-linux.pre-commit.enabledPackages;
 
-  packages = (with pkgs; [
+  packages = with pkgs; [
     agenix
     xdg-ninja
     nix-init
     nh
     fup-repl
     snowfallorg.flake
-  ]) ++ (with inputs.nix-update.packages; [
-    nix-update
-  ]);
+  ];
 }
