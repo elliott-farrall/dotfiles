5d7064acebfb944ed695ac008c2c4eacb46f7c62
ElliottSullingeFarrall <108588212+ElliottSullingeFarrall@users.noreply.github.com>
2024-07-11 19:28:28 +0100

    elliotts-laptop (2264)
    - Disable Pipewire functionality by commenting out the `services.pipewire` configuration
    - Enable PulseAudio functionality by uncommenting the `hardware.pulseaudio.enable` line in the audio default configuration file

--- systems/x86_64-linux/elliotts-laptop/config/audio/default.nix
+++ systems/x86_64-linux/elliotts-laptop/config/audio/default.nix
@@ -2,14 +2,10 @@
 }:
 
 {
-  # sound.enable = true;
-  hardware.pulseaudio.enable = false;
-  # security.rtkit.enable = true;
-  services.pipewire = {
-    enable = true;
-    # alsa.enable = true;
-    # alsa.support32Bit = true;
-    pulse.enable = true;
-    # wireplumber.enable = true;
-  };
+  # PulseAudio
+  hardware.pulseaudio.enable = true;
+  # services.pipewire = {
+  #   enable = true;
+  #   pulse.enable = true;
+  # };
 }
