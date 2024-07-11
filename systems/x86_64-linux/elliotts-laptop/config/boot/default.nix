30cc6bc0ecd5117451c637801906ec22f913dfc5
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-11 15:22:11 +0100

    chore: Update boot configurations and disable unused kernel parameters
    - Commented out code related to `systemd.services.greetd` in `default.nix`

--- systems/x86_64-linux/elliotts-laptop/config/boot/default.nix
+++ systems/x86_64-linux/elliotts-laptop/config/boot/default.nix
@@ -13,17 +13,17 @@
     initrd.verbose = false;
 
     plymouth.enable = true;
-    systemd.services.greetd = {
-      overrideStrategy = "asDropin";
-      unitConfig = {
-        Conflicts = [ "plymouth-quit.service" ];
-        After = [ "plymouth-quit.service" "rc-local.service" "plymouth-start.service" "systemd-user-sessions.service" ];
-        OnFailure = [ "plymouth-quit.service" ];
-      };
-      serviceConfig = {
-        ExecStartPost = [ "-${pkgs.coreutils}/bin/sleep 30" "-${pkgs.plymouth}/bin/plymouth quit --retain-splash" ];
-      };
-    };
+    # systemd.services.greetd = {
+    #   overrideStrategy = "asDropin";
+    #   unitConfig = {
+    #     Conflicts = [ "plymouth-quit.service" ];
+    #     After = [ "plymouth-quit.service" "rc-local.service" "plymouth-start.service" "systemd-user-sessions.service" ];
+    #     OnFailure = [ "plymouth-quit.service" ];
+    #   };
+    #   serviceConfig = {
+    #     ExecStartPost = [ "-${pkgs.coreutils}/bin/sleep 30" "-${pkgs.plymouth}/bin/plymouth quit --retain-splash" ];
+    #   };
+    # };
 
     kernelParams = [
       "boot.shell_on_fail"
