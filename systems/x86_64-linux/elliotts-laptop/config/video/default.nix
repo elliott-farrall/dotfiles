78ad75c2b6f6bbcc5bd5685f45cffaad4c431606
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-11 16:55:31 +0100

    chore: Update video configuration for Hyprland in Wayland
    - Enable hardware graphics in the video configuration

--- systems/x86_64-linux/elliotts-laptop/config/video/default.nix
+++ systems/x86_64-linux/elliotts-laptop/config/video/default.nix
@@ -1,30 +1,22 @@
-{ pkgs
+{ lib
 , ...
 }:
 
 {
-  # hardware.graphics = {
-  #   enable = true;
-  #   enable32Bit = true;
-  #   extraPackages = with pkgs; [
-  #     intel-compute-runtime
-  #   ];
-  # };
-
-  services.xserver = {
-    # enable = true;
-    excludePackages = with pkgs; [ xterm ];
-    # xkb.layout = "gb";
+  hardware.graphics = {
+    enable = true;
+    # enable32Bit = true;
+    # extraPackages = with pkgs; [
+    #   intel-compute-runtime
+    # ];
   };
 
   boot.loader.grub.gfxmodeEfi = "2256x1504";
 
-  home-manager.sharedModules = [
-    {
-      wayland.windowManager.hyprland.settings.monitor = [
-        "eDP-1, 2256x1504@60, auto, 1.333333"
-        ", preferred, auto, auto"
-      ];
-    }
-  ];
+  home-manager.sharedModules = lib.singleton {
+    wayland.windowManager.hyprland.settings.monitor = [
+      "eDP-1, 2256x1504@60, auto, 1.333333"
+      ", preferred, auto, auto"
+    ];
+  };
 }
