970365eed4992e82b415ab2f6e6ca3aa35965a4d
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-11 16:04:07 +0100

    Update boot configuration and enable services
    - Update kernelParams in the boot section of default.nix configuration file

--- systems/x86_64-linux/elliotts-laptop/config/boot/default.nix
+++ systems/x86_64-linux/elliotts-laptop/config/boot/default.nix
@@ -9,8 +9,18 @@
   ];
 
   boot = {
-    # consoleLogLevel = 0;
-    # initrd.verbose = false;
+    kernelParams = [
+      "boot.shell_on_fail" # Allows for root shell if failure to boot. Requires root password.
+      "i915.fastboot=1"
+    ];
+
+    loader = {
+      grub = {
+        enable = true;
+        device = "nodev";
+      };
+      timeout = lib.mkDefault 3;
+    };
 
     plymouth.enable = true;
     # systemd.services.greetd = {
@@ -24,27 +34,5 @@
     #     ExecStartPost = [ "-${pkgs.coreutils}/bin/sleep 30" "-${pkgs.plymouth}/bin/plymouth quit --retain-splash" ];
     #   };
     # };
-
-    kernelParams = [
-      "boot.shell_on_fail" # Allows for root shell if failure to boot. Requires root password.
-      # "i915.fastboot=1"
-
-      # "splash" # Enable splash screen
-      # "bgrt_disable" # Do not display the OEM logo after loading the ACPI tables
-
-      # "quiet" # Disable kernel messages
-      # "loglevel=3"
-      # "udev.log_level=3"
-      # "udev.log_priority=3"
-      # "systemd.show_status=false"
-    ];
-
-    loader = {
-      grub = {
-        enable = true;
-        device = "nodev";
-      };
-      timeout = lib.mkDefault 3;
-    };
   };
 }
