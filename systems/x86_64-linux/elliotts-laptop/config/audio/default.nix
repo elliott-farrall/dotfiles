ffecf759d4b79c0416ae65b24ece651c6358e56d
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-11 18:10:51 +0100

    chore: Update audio and waybar configuration in hyprwm package
    - Enable `pipewire` service for audio
    - Update `waybar` module in `hyprwm` package configuration

--- systems/x86_64-linux/elliotts-laptop/config/audio/default.nix
+++ systems/x86_64-linux/elliotts-laptop/config/audio/default.nix
@@ -5,11 +5,11 @@
   sound.enable = true;
   hardware.pulseaudio.enable = false;
   # security.rtkit.enable = true;
-  # services.pipewire = {
-  #   enable = true;
-  #   alsa.enable = true;
-  #   alsa.support32Bit = true;
-  #   pulse.enable = true;
-  #   wireplumber.enable = true;
-  # };
+  services.pipewire = {
+    enable = true;
+    # alsa.enable = true;
+    # alsa.support32Bit = true;
+    pulse.enable = true;
+    # wireplumber.enable = true;
+  };
 }
