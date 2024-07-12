5907ce90ddc2816097eb4a36ae5a35a5ec92aee0
ElliottSullingeFarrall <elliott.chalford@gmail.com>
2024-07-12 11:43:12 +0100

    chore: Update default.nix config for kmscon service
    - Enable `kmscon` service in `default.nix` config file
    - Comment out `hwRender` option in `kmscon` service configuration

--- systems/x86_64-linux/elliotts-laptop/config/boot/default.nix
+++ systems/x86_64-linux/elliotts-laptop/config/boot/default.nix
@@ -24,9 +24,9 @@
     plymouth.enable = true;
   };
 
-  # services.kmscon = {
-  #   enable = true;
-  #   hwRender = true;
-  # };
+  services.kmscon = {
+    enable = true;
+    # hwRender = true;
+  };
   # systemd.services."getty@".enable = false;
 }
