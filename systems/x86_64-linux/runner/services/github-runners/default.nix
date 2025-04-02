1265de4f87577b190503b44f0fdc290e1a5a144a
elliott-farrall <108588212+elliott-farrall@users.noreply.github.com>
2025-04-02 14:46:59 +0100

    chore: update github-runner

--- systems/x86_64-linux/runner/services/github-runners/default.nix
+++ systems/x86_64-linux/runner/services/github-runners/default.nix
@@ -6,10 +6,6 @@
 {
   age.secrets."dotfiles/token".file = ./repos/dotfiles.age;
 
-  # system.activationScripts.setupAuthorizedKeys = ''
-  #   cat /etc/ssh/authorized_keys.d/root >> /root/.ssh/authorized_keys
-  # '';
-
   services.github-nix-ci = {
     personalRunners."elliott-farrall/dotfiles" = {
       tokenFile = config.age.secrets."dotfiles/token".path;
