78e287aac57cf9b7eb2e1c51f87adb533e222192
elliott-farrall <108588212+elliott-farrall@users.noreply.github.com>
2025-04-06 23:03:26 +0100

    feat: expose grafana sever on runner

--- systems/x86_64-linux/runner/services/metrics/default.nix
+++ systems/x86_64-linux/runner/services/metrics/default.nix
@@ -1,8 +1,24 @@
-{ ...
+{ config
+, ...
 }:
 
+let
+  inherit (config.services.grafana.settings) server;
+in
 {
-  services.grafana.enable = true;
+  services.nginx.enable = true;
+  networking.firewall.allowedTCPPorts = [ 80 ];
+
+  services.grafana = {
+    enable = true;
+    settings.server.domain = "runner.main.dotfiles.elliott-farrall.garnix.me";
+  };
+  services.nginx.virtualHosts.${server.domain} = {
+    locations."/" = {
+      proxyPass = "http://${toString server.http_addr}:${toString server.http_port}";
+      proxyWebsockets = true;
+    };
+  };
 
   services.prometheus.enable = true;
 }
