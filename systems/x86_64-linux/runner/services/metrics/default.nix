b7fd412c40d9a5a8d15715717f7e545066b48f9c
elliott-farrall <108588212+elliott-farrall@users.noreply.github.com>
2025-04-08 11:03:37 +0100

    feat: updates to metrics on runner

--- systems/x86_64-linux/runner/services/metrics/default.nix
+++ systems/x86_64-linux/runner/services/metrics/default.nix
@@ -2,9 +2,6 @@
 , ...
 }:
 
-let
-  inherit (config.services.grafana.settings) server;
-in
 {
   services.nginx.enable = true;
   networking.firewall.allowedTCPPorts = [ 80 ];
@@ -12,13 +9,37 @@ in
   services.grafana = {
     enable = true;
     settings.server.domain = "runner.main.dotfiles.elliott-farrall.garnix.me";
+
+    provision = {
+      enable = true;
+
+      datasources.settings.datasources = [
+        {
+          name = "Prometheus";
+          type = "prometheus";
+          url = "http://${config.services.prometheus.listenAddress}:${toString config.services.prometheus.port}";
+        }
+      ];
+    };
   };
-  services.nginx.virtualHosts.${server.domain} = {
+  services.nginx.virtualHosts.${config.services.grafana.settings.server.domain} = {
     locations."/" = {
-      proxyPass = "http://${toString server.http_addr}:${toString server.http_port}";
+      proxyPass = "http://${toString config.services.grafana.settings.server.http_addr}:${toString config.services.grafana.settings.server.http_port}";
       proxyWebsockets = true;
+      recommendedProxySettings = true;
     };
   };
 
-  services.prometheus.enable = true;
+  services.prometheus = {
+    enable = true;
+
+    scrapeConfigs = [
+      {
+        job_name = "comin";
+        static_configs = [{
+          targets = [ "127.0.0.1:${toString config.services.comin.exporter.port}" ];
+        }];
+      }
+    ];
+  };
 }
