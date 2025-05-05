9d9cbd811d9d3160a912eca13413b7308dcbbb2e
elliott-farrall <108588212+elliott-farrall@users.noreply.github.com>
2025-05-05 22:12:58 +0100

    chore: remove old firewall rules for lima

--- systems/x86_64-linux/lima/config/networking/default.nix
+++ systems/x86_64-linux/lima/config/networking/default.nix
@@ -1,12 +1,9 @@
 { ...
 }:
 
-#TODO - Cleanup lima networking config
-
 {
   networking = {
     networkmanager.enable = true;
-    firewall.allowedTCPPorts = [ 80 443 8443 ];
     firewall.allowedUDPPorts = [ 5901 ]; # rmview
   };
 }
