30930a7ce63ec834849b04e44a51d60e5a67874f
elliott-farrall <108588212+elliott-farrall@users.noreply.github.com>
2025-05-06 18:01:46 +0100

    refactor: docker login service

--- modules/nixos/virtualisation/docker/default.nix
+++ modules/nixos/virtualisation/docker/default.nix
@@ -9,6 +9,25 @@
 let
   cfg = config.virtualisation;
   enable = cfg.podman.enable || cfg.docker.enable;
+
+  mkLoginService = backend: {
+    description = "${lib.internal.capitalise backend} Login Service";
+    after = [ "network-online.target" ];
+    requires = [ "network-online.target" ];
+
+    path = [
+      cfg.${backend}.package
+      pkgs.toybox
+    ];
+
+    script = ''
+      until ping -4qc 1 login.docker.com; do sleep 1; done
+      cat ${config.age.secrets."docker/password".path} | ${backend} login --username $(cat ${config.age.secrets."docker/username".path}) --password-stdin
+    '';
+
+    wantedBy = [ "multi-user.target" ];
+    serviceConfig.Restart = "on-failure";
+  };
 in
 {
   config = {
@@ -17,42 +36,9 @@ in
       "docker/password".file = ./password.age;
     };
 
-    systemd.services.docker-login = lib.mkIf cfg.docker.enable {
-      description = "Docker Login Service";
-      after = [ "network-online.target" ];
-      requires = [ "network-online.target" ];
-
-      path = with pkgs; [
-        docker
-        toybox
-      ];
-
-      script = ''
-        until ping -qc 1 registry-1.docker.io; do sleep 1; done
-        cat ${config.age.secrets."docker/password".path} | docker login --username $(cat ${config.age.secrets."docker/username".path}) --password-stdin
-      '';
-
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig.Restart = "on-failure";
-    };
-
-    systemd.services.podman-login = lib.mkIf cfg.podman.enable {
-      description = "Podman Login Service";
-      after = [ "network-online.target" ];
-      requires = [ "network-online.target" ];
-
-      path = with pkgs; [
-        podman
-        toybox
-      ];
-
-      script = ''
-        until ping -qc 1 registry-1.docker.io; do sleep 1; done
-        cat ${config.age.secrets."docker/password".path} | podman login --username $(cat ${config.age.secrets."docker/username".path}) --password-stdin
-      '';
-
-      wantedBy = [ "multi-user.target" ];
-      serviceConfig.Restart = "on-failure";
+    systemd.services = {
+      docker-login = lib.mkIf cfg.docker.enable (mkLoginService "docker");
+      podman-login = lib.mkIf cfg.podman.enable (mkLoginService "podman");
     };
 
     virtualisation.podman.dockerSocket.enable = cfg.podman.enable;
