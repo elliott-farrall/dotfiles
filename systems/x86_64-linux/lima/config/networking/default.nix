824f5a08aebdca6d004624bf4e9b1b3c0fec1491
elliott-farrall <108588212+elliott-farrall@users.noreply.github.com>
2025-08-01 10:09:52 +0100

    feat: add zerotier vpn for lima

--- systems/x86_64-linux/lima/config/networking/default.nix
+++ systems/x86_64-linux/lima/config/networking/default.nix
@@ -4,6 +4,11 @@
 {
   networking.networkmanager.enable = true;
 
+  services.zerotierone = {
+    enable = true;
+    joinNetworks = [ "561b011a2752dfda" ];
+  };
+
   services.avahi = {
     enable = true;
     nssmdns4 = true;
